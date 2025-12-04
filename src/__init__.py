ddeb7e8bf3e465bb296616a83bcb6ef6b721cec4
Nizar Channane <nizar.channane@6wind.com>
2025-12-04 17:03:07 +0100

    fix: clean up architecture following rebase mistake.

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Flask Image Tracker Server Package
-
-This package contains the Flask application for tracking client access
-through image requests and logging detailed client information.
-"""
-__version__ = '1.0.0'
